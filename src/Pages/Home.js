8cf9fac3528376a094181c70b2ab6fb175169f5a
Rodrigo Macedo <rodrigomac3do@gmail.com>
2022-05-24 19:17:47 -0300

    Adiciona componente Categories na Home

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Categories from '../components/Categories';
 
 class Home extends React.Component {
   render() {
     return (
       <>
+        <Categories />
         <form>
           <label htmlFor="busca">
             <input type="text" id="busca" />
