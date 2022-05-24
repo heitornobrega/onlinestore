5b2ec0020376011541c6898af03693e7ef572bcc
Rodrigo Macedo <rodrigomac3do@gmail.com>
2022-05-24 17:56:40 -0300

    Criação de lista de categorias

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import Categories from './components/Categories';
 import Home from './Pages/Home';
 
 function App() {
   return (
-    <Home />
+    <>
+      <Home />
+      <Categories />
+    </>
   );
 }
 
