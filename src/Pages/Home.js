81380cab34bbd6c023bc8586619b314023caa36d
heitornobrega <nobheitor@gmail.com>
2022-05-24 15:31:49 -0300

    req 02

---
+++ src/Pages/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class Home extends React.Component {
+  render() {
+    return (
+      <>
+        <form>
+          <label htmlFor="busca">
+            <input type="text" id="busca" />
+          </label>
+        </form>
+        <p data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </p>
+      </>
+    );
+  }
+}
+
+export default Home;
